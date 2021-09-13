150d2fac58476bd6c6d021a41a1f0638c31c94d7
Vedhashni V <vedhashni.v@tvsnext.io>
2021-09-13 23:26:11 +0530

    [Vedhashni] Updated the file with refactored the code with pagefactory and handle notification

---
+++ DataDrivenForFB/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace DataDrivenForFB
+{
+    public class LoginPage
+    {
+        public LoginPage(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "email")]
+        [CacheLookup]
+        public IWebElement email;
+
+        [FindsBy(How = How.Id, Using = "pass")]
+        [CacheLookup]
+        public IWebElement password;
+
+        [FindsBy(How = How.Name, Using = "login")]
+        [CacheLookup]
+        public IWebElement loginbutton;
+    }
+}
--- DataDrivenForFB/UnitTest1.cs
+++ DataDrivenForFB/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Chrome;
 
 namespace DataDrivenForFB
 {
@@ -10,15 +11,21 @@ namespace DataDrivenForFB
        public void ReadExcelData()
         {
             ExcelOperations excel = new ExcelOperations();
-            excel.PopulateFromExcel(@"C:\Users\vedhashni.v\Documents\Data.xlsx");
-            driver.FindElement(By.Name("email")).SendKeys(excel.ReadData(1,"Email"));
+            LoginPage login = new LoginPage(driver);
+            excel.PopulateFromExcel(@"C:\Users\vedhashni.v\source\repos\DataDrivenForFB\DataDrivenForFB\Data.xlsx");
+            login.email.SendKeys(excel.ReadData(1,"Email"));
             System.Threading.Thread.Sleep(1000);
-            driver.FindElement(By.Id("pass")).SendKeys(excel.ReadData(1, "Password"));
+            login.password.SendKeys(excel.ReadData(1, "Password"));
             System.Threading.Thread.Sleep(1000);
-            driver.FindElement(By.Name("login")).Click();
+            login.loginbutton.Click();
             System.Threading.Thread.Sleep(10000);
-            Actions notify = new Actions(driver);
-            notify.SendKeys(Keys.Escape).Build().Perform();
+            //var notification = driver.SwitchTo().Alert();
+            //notification.Accept();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("disable-popup-blocking");
+            options.AddArgument("--headless");
+            //Actions notify = new Actions(driver);
+            //notify.SendKeys(Keys.Escape).Build().Perform();
             System.Threading.Thread.Sleep(10000);
         }
     }
